69b1d5a3e662c8c9b4ac9a8279108e928cffd88f
Николай Моденов <uptb.modenov@yandex.ru>
2024-05-20 21:15:53 +0300

    add redoc in urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include, path
-from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 
 from api.views import (
@@ -14,11 +13,6 @@ router.register('ingredients', ProductViewSet, basename='ingredients')
 router.register('users', FoodgramUserViewSet, basename='users')
 
 urlpatterns = [
-    path(
-        'docs/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
     path('auth/', include('djoser.urls.authtoken')),
     path('', include(router.urls)),
 ]
