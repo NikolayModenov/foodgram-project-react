f3660ef7908430d0a44d047f727b777f236b490b
Николай Моденов <uptb.modenov@yandex.ru>
2024-05-23 00:12:02 +0300

    the fifth preview

--- base.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO recipe_tag VALUES(1, 'завтрак', '#E26C2D', 'zavtrak');         
-INSERT INTO recipe_tag VALUES(2, 'обед', '#E26C2D', 'obed');
-INSERT INTO recipe_product VALUES(1, 'соль', 'кг');
-INSERT INTO recipe_product VALUES(2, 'сахар', 'т');
